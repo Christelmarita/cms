4fe05eb5b5722c51e75a8fd6acd9fbffc67473cf
Christelmarita <christelmarita.oe@gmail.com>
2023-04-22 21:05:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 const url = `https://cms.onibodesign.no/wp-json/wc/store/products/${id}`;
+const featuredURL = `https://cms.onibodesign.no/products/${id}?&featured=true`;
 
 
 // REGULAR PRODUCTS //
@@ -96,8 +97,70 @@ async function getFeaturedProducts() {
     return featuredProducts;
 }
 
-async function getSingleFeatProduct(id) {
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
+async function getFeaturedProduct(id) {
+    const response = await fetch(featuredURL);
+    const featuredResult = await response.json();
+    return featuredResult;
 }
+
+function createFeaturedProductHTML(featuredProduct) {
+    const container = document.querySelector(".featured-games");
+    const productContainer = document.createElement("div");
+    productContainer.classList.add("card");
+    productContainer.id = featuredProduct.id;
+
+    for (let i = 0; i < featuredProduct.images.length; i++) {;
+        const imgData = featuredProduct.images[i];
+        const img = document.createElement("img");
+        img.classList.add("card-image");
+        img.src = imgData.src;
+        img.alt = imgData.alt;
+        productContainer.append(img)
+    }
+
+
+    const title = document.createElement("p");
+    title.innerText = featuredProduct.name;
+    title.classList.add("product_name");
+    productContainer.append(title);
+
+    const console = document.createElement("p");
+    console.innerText = featuredProduct.attributes[0].terms[0].name;
+    console.classList.add("product_console");
+    productContainer.append(console);
+
+    const price = document.createElement("p");
+    price.innerText = featuredProduct.prices.price/100;
+    price.classList.add("product_price");
+    productContainer.append(price);
+
+    const link = document.createElement("a");
+    link.innerText = `Read more`;
+    link.classList.add("product_link");
+    link.href = `../products/details.html?id=${featuredProduct.id}`;
+    productContainer.append(link);
+
+    const button = document.createElement(`button`);
+    button.innerText = `Add to basket`;
+    button.classList.add("add-to-basket");
+    button.addEventListener('click', () => {
+        alert('The product is successfully added to your basket!')
+    })
+    productContainer.append(button);
+
+    container.append(productContainer);
+}
+
+function createFeaturedProductsHTML(featuredProducts) {
+    for (let i = 0; i < featuredProducts.length; i++) {
+        const featuredProduct = featuredProducts[i];
+        createFeaturedProductHTML(featuredProduct);
+    }
+}
+
+async function mainFeatured() {
+    const featuredProducts = await getFeaturedProducts();
+    createFeaturedProductsHTML(featuredProducts);
+}
+
+mainFeatured()
